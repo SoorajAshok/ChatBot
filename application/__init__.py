0ded4fbe86ff085077e0c92a2002e25a9c4ed9eb
SOORAJ_SRJ <46915809+SoorajAshok@users.noreply.github.com>
2022-08-25 23:25:35 +0530

    Updated Data.yml
    Updated Data.yml

--- application/__init__.py
+++ application/__init__.py
@@ -8,12 +8,9 @@ app = Flask(__name__)
 bot = ChatBot("Wall-E", storage_adapter='chatterbot.storage.SQLStorageAdapter',
     database_uri='sqlite:///database.sqlite3')
 trainer = ChatterBotCorpusTrainer(bot)
-#trainer.train(conversation)
 trainer.train("chatterbot.corpus.english")
 
-#import os
-#conversation = open('application/data/data.yml').read()
-#trainer.train(conversation)
+
 trainer.train('application/data/data.yml')
 
 
