aca0b75ffba5833474adec89a3c02c5f78b3d0e9
SOORAJ_SRJ <46915809+SoorajAshok@users.noreply.github.com>
2022-08-26 11:05:22 +0530

    pip requirements

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
